d1a52fee97a51e349703c9d0f68e4eddbf4bc054
Mark Glushko <markableglushko@gmail.com>
2019-03-27 22:24:34 +0300

    Немножко подправил триггер

--- Script/TR_before_ins_players.sql
+++ Script/TR_before_ins_players.sql
@@ -4,7 +4,7 @@ DELIMITER //
 create trigger  tr_before_ins_players before insert on players
 for each row
 begin
-	
+	declare message char(100);
     declare f int;
     declare pl_name char(100);
     set pl_name = new.name;
@@ -15,7 +15,9 @@ begin
     where p.name = pl_name;
     
     if  f is not null then 
-		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Warning: player have already exist';
+		select Concat('Warning: player have already exist ()',' ', new.name)
+        into message;
+		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = message;
     end if;
 
 end//
