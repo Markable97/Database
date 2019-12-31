8fcfdf63b50fcb8f3581839cc0ac5810e50c6fea
Mark Glushko <markableglushko@gmail.com>
2019-12-31 11:13:47 +0300

    подведение итогов года. Восточная лига!

--- Script/P_insPlayerInMatche2.sql
+++ Script/P_insPlayerInMatche2.sql
@@ -1,11 +1,11 @@
 drop procedure if exists insPlayerInMatche2;
 DELIMITER //
 create procedure insPlayerInMatche2(
-	in t_home varchar(50),
-    in t_guest varchar(50),
+	in t_home varchar(100),
+    in t_guest varchar(100),
     in tour int,
-    in name_player varchar(50),
-    in t_player varchar(50),
+    in name_player varchar(200),
+    in t_player varchar(100),
     in goals int,
     in assists int,
     in c_yellow int,
@@ -13,8 +13,9 @@ create procedure insPlayerInMatche2(
     in l_penalty int,
     in l_penalty_out int,
     in l_own_goal int,
-    in l_url_player varchar(50),
-    in player_team varchar(50))
+    in l_url_player varchar(200),
+    in player_team varchar(100),
+    in l_amplua varchar(50))
 begin
 	declare id_player_auto_before int;
     declare id_player_auto_after int;
@@ -26,16 +27,17 @@ begin
     select id_match
      into l_id_match
     from v_matches
-    where team_home like change_team_name(t_home) and team_guest like change_team_name(t_guest) and id_tour = tour;
+    where team_home like change_team_name(t_home) and team_guest like change_team_name(t_guest) and id_tour = tour
+    and played = 1;
     if l_id_match is null then 
-		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Error: Not found match id!!!!!';
+		SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Error: Not found match id and match played!!!!!';
     end if;
     #вытаскиваем игрока
     select id_player
      into l_id_player
 	from players p
      where p.url_site like l_url_player; 
-     
+    
     #добавляем игрока в матч
     if l_id_player is not null then 
 		insert into players_in_match
@@ -49,25 +51,30 @@ begin
 				penalty = l_penalty,
 				penalty_out = l_penalty_out,
 				own_goal = l_own_goal;
+       #проставляем амплуа
+       update players
+       set id_amplua = (select id_amplua from amplua where name_amplua = l_amplua)
+       where id_player = l_id_player;
 	else 
 		#SIGNAL SQLSTATE '02000' SET MESSAGE_TEXT = 'Warning: Player is not found';
         
         SELECT `AUTO_INCREMENT`
         into id_player_auto_before
 		FROM  INFORMATION_SCHEMA.TABLES
-		WHERE TABLE_SCHEMA = 'football_main_realese'
+		WHERE TABLE_SCHEMA = 'football_main_ucl'
 		AND   TABLE_NAME   = 'players';
 			
         insert into players
         set name = name_player, 
 			id_team = (select id_team from teams t where t.team_name = change_team_name(replace(player_team, '-Д', ''))),
-            url_site = l_url_player;
+            url_site = l_url_player,
+            id_amplua = (select id_amplua from amplua where name_amplua = l_amplua);
             
 		
         SELECT `AUTO_INCREMENT`
         into id_player_auto_after
 		FROM  INFORMATION_SCHEMA.TABLES
-		WHERE TABLE_SCHEMA = 'football_main_realese'
+		WHERE TABLE_SCHEMA = 'football_main_ucl'
 		AND   TABLE_NAME   = 'players';
         
         if id_player_auto_before < id_player_auto_after then 
